0038d523a5a73c5aa1043a0b950a44c9b6c524ff
chloekh <chloekh@stud.ntnu.no>
2023-03-10 14:55:04 +0100

    Den ekke noe fin men den funker :D

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
+
 
 function App() {
+
+  const structureInputRef = useRef();
+  const [structure, setStructure] = useState("");
+  const [resultStructure, setResultStructure] = useState("");
+
+  async function submitHandler(event) {
+    event.preventDefault();
+    const enteredStructure = structureInputRef.current.value;
+    const structureData = {
+      sequence: enteredStructure
+    }
+    const response =  await fetch(' http://127.0.0.1:8000/sequence', {
+                            method: 'POST',                                
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(structureData),
+                        }).then(async (res) => {
+                            const data = await res.json()
+                            console.log(data)
+                            setResultStructure(data.structure)
+                        })
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={submitHandler}>
+          <label>Enter your structure:
+            <input 
+              type="text" 
+              ref={structureInputRef}
+              onChange={(e) => setStructure(e.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
+        <h2>Result: </h2>
+        <h4>{resultStructure}</h4>
       </header>
     </div>
   );
 }
 
 export default App;
+
